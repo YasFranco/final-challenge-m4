8b3dbab65298bb906c09c6bc9e719c1c5f044a53
yasminfranco <yasfranco98@gmail.com>
2024-07-24 12:47:09 -0300

    Conexion con la API elegida

---
+++ src/constants.ts
@@ -0,0 +1,2 @@
+export const URL_API = "https://api.disneyapi.dev/";
+export const PORT = 5000;
---
+++ src/database/database.ts
@@ -0,0 +1,12 @@
+import { URL_API } from "../constants";
+
+
+async function getData(ruta: string){
+    const response = await fetch(URL_API + ruta);
+    const data = await response.json();
+    console.log(data)
+
+    return data;
+}
+
+export {getData}
