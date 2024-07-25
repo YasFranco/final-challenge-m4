a075ba69643a40d335a38276b2c0b87a75fd5951
yasminfranco <yasfranco98@gmail.com>
2024-07-25 13:48:26 -0300

    Creación del archivo para el historial de busqueda

--- src/model/model.ts
+++ src/model/model.ts
@@ -1,40 +1,65 @@
 import { getData } from "../database/database";
 import { Messages } from "../utils/messages";
+import fs from "fs";
+import path from "path";
+
+const fileText = path.resolve(__dirname, "../database/history.json");
+
+const saveInfo = async (usedMethod:string, data:any) => {
+    try {
+        let history = [];
+
+        if(fs.existsSync(fileText)){
+            const content = fs.readFileSync(fileText, "utf-8");
+            history = JSON.parse(content);
+        }
+
+        history.push({method:usedMethod, data});
+        fs.writeFileSync(fileText, JSON.stringify(history));
+
+    } catch (error) {
+        console.log("Failure to save history", error)
+    }
+}
 
 class DisneyModel {
 
-    async  getAllCharacters () {
+    async getAllCharacters() {
         const character = await getData("character")
         const characters = character.data
+        await saveInfo("All the characters:", characters);
+
         return characters
     }
 
-    async getCharactersById (id:number) {
+    async getCharactersById(id: number) {
         const character = await getData("character")
         const characters = character.data
 
-        const foundCharacter = characters.find((c:any) => c._id === id)
-        if(!foundCharacter){
-            return { data: null, message: Messages.NOT_FOUND}
-        }else {
-             return foundCharacter
+        const foundCharacter = characters.find((c: any) => c._id === id)
+        if (!foundCharacter) {
+            return { data: null, message: Messages.NOT_FOUND }
+        } else {
+            await saveInfo(`Character by ID: ${id} `, foundCharacter)
+            return foundCharacter
         }
     }
 
-    async getCharacterByName ( name: string){
+    async getCharacterByName(name: string) {
         const character = await getData("character")
         const characters = character.data
 
         const nameLowerCase = name.toLowerCase();
-        const searchByName = characters.filter((c:any) => c.name.toLowerCase().includes(nameLowerCase))
+        const searchByName = characters.filter((c: any) => c.name.toLowerCase().includes(nameLowerCase))
 
-        if(!searchByName){
-            return { data: null, message: Messages.NOT_FOUND};
-        }else {
-             return searchByName;
+        if (!searchByName) {
+            return { data: null, message: Messages.NOT_FOUND };
+        } else {
+            await saveInfo(`Character by name: ${name} `, searchByName)
+            return searchByName;
         }
     }
-    
+
 
 }
 
