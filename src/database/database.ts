144ec2cbf78833e414841fdf25a8b308cebfae58
yasminfranco <yasfranco98@gmail.com>
2024-07-25 14:10:12 -0300

    Revision final

--- src/database/database.ts
+++ src/database/database.ts
@@ -4,7 +4,7 @@ import { URL_API } from "../constants";
 async function getData(ruta: string){
     const response = await fetch(URL_API + ruta);
     const data = await response.json();
-    // console.log(data)
+    
 
     return data;
 }
